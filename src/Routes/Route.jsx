0d70cf7490b55768383591a7dce6a850e39ca6e6
abrarazmain <ajmainabrar5@gmail.com>
2023-08-18 20:55:38 +0600

    test

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,26 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../Layouts/MainLayouts";
 import Home from "../Pages/Home/Home/Home";
-import Progress from "../Pages/Home/Home/Learn/Progress";
-
 
 
 const router = createBrowserRouter([
-    {
-        path:"/",
-        element: <MainLayouts/>,
-        children:[
-            {
-               path:"/",
-               element:<Home/>
-            },
-            {
-                path: 'learn',
-                element:<Progress></Progress>
-            }
-        ]
-    }
-])
-
+  {
+    path: "/",
+    element: <MainLayouts />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+   
+    ],
+  },
+]);
 
 export default router;
--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -8,15 +8,27 @@ const NavBar = () => {
     setLanguage(event.target.value);
   };
 
+  const textContent = {
+    en: {
+      selectLanguage: "Select Your language:",
+    },
+    bn: {
+      selectLanguage: "আপনার ভাষা নির্বাচন করুন:",
+    },
+    hi: {
+      selectLanguage: "अपनी भाषा का चयन करें:",
+    },
+  };
+
   return (
     <div className="container mx-auto lg:px-16 flex items-center justify-between lg:gap-96 sticky top-0">
       <h2 className="lg:text-2xl font-primary lg:ms-36">Vocab Master</h2>
 
       <div className="flex items-center flex-1 font-primary text-gray-500 lg:text-1xl">
-        <p>Select Your language:</p>
+        <p>{textContent[language].selectLanguage}</p>
 
         <select
-          className="select w-1/3 max-w-xs lg:text-1xl"
+          className="select w-1/6 max-w-xs lg:text-1xl m-4"
           value={language} // Set the selected value based on the context
           onChange={handleLanguageChange} // Call the function to update the language
         >
